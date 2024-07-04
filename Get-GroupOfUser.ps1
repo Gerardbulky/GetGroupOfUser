90e945c382972ade041a4ae9c31de930b2107a1f
Gerardbulky <gerardambe@yahoo.com>
2024-07-04 12:07:00 +0200

    Added Readme file

--- Get-GroupOfUser.ps1
+++ Get-GroupOfUser.ps1
@@ -1,18 +1,4 @@
-﻿
-
-<#
-.SYNOPSIS
-    Gets the groups info related to a user
-.DESCRIPTION
-    This function checks if the user exists or not, and if exists returns the groups information he is part of.
-.PARAMETER UserId
-    The userId of the user for which groups need to be fetched
-.EXAMPLE
-    Get-GroupOfUser -UserId "userId"
-#> 
-
-
-function Get-GroupOfUser {
+﻿function Get-GroupOfUser {
     param(
         [Parameter(Mandatory = $true)][string]$UserId
     )
